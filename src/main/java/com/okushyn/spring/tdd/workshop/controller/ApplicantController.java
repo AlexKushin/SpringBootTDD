f6d70f1f5fc314733ee8c71d6dca1e5354300d31
kyshin <kyshin1993@gmail.com>
2025-03-17 18:56:03 +0100

    @Valid annotation has been added to pass the validation test

--- src/main/java/com/okushyn/spring/tdd/workshop/controller/ApplicantController.java
+++ src/main/java/com/okushyn/spring/tdd/workshop/controller/ApplicantController.java
@@ -2,6 +2,7 @@ package com.okushyn.spring.tdd.workshop.controller;
 
 import com.okushyn.spring.tdd.workshop.model.Applicant;
 import com.okushyn.spring.tdd.workshop.service.ApplicantService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,7 @@ public class ApplicantController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<Void> createApplicant(final @RequestBody Applicant applicant) throws Exception {
+    public ResponseEntity<Void> createApplicant(final @Valid @RequestBody Applicant applicant) throws Exception {
         final Applicant savedApplicant = applicantService.save(applicant);
         return ResponseEntity.created(new URI("applicants/" + savedApplicant.getApplicantId())).build();
     }
