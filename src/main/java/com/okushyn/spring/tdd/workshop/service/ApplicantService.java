edc46c3020d5e07c6c7278972111540ce1fe1e96
kyshin <kyshin1993@gmail.com>
2025-03-19 19:36:00 +0100

    logic for unhappy path when applicant is already exists has been implemented in ApplicantService

--- src/main/java/com/okushyn/spring/tdd/workshop/service/ApplicantService.java
+++ src/main/java/com/okushyn/spring/tdd/workshop/service/ApplicantService.java
@@ -1,11 +1,33 @@
 package com.okushyn.spring.tdd.workshop.service;
 
+import com.okushyn.spring.tdd.workshop.exceptions.ApplicantAlreadyExistsException;
 import com.okushyn.spring.tdd.workshop.model.Applicant;
+import com.okushyn.spring.tdd.workshop.model.ContactPoint;
+import com.okushyn.spring.tdd.workshop.model.ElectronicAddress;
+import com.okushyn.spring.tdd.workshop.repository.ApplicantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ApplicantService {
+
+    @Autowired
+    ApplicantRepository repository;
+
     public Applicant save(Applicant applicant) {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        Optional<Applicant> byEmail = repository.findByEmail(Optional.of(applicant)
+                .map(Applicant::getContactPoint)
+                .map(ContactPoint::getElectronicAddress)
+                .map(ElectronicAddress::getEmail)
+                .orElseThrow());
+
+        if (byEmail.isPresent()) {
+            throw new ApplicantAlreadyExistsException("Applicant already exists");
+        }
+
+        return repository.save(applicant);
     }
 }
